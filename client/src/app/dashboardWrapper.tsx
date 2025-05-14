f796032e70611ddf2223154478fe6db4f23b4ae6
Luis Mendoza <luis.mendoza9122#@gmail.com>
2025-05-13 19:09:43 -0600

    Initial Creation

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
+
+
+type Props = {}
+
+const DashboardWrapper = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+        {/* Sidebar */}
+        <Sidebar />
+        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>        
+            <Navbar/>
+            {children}
+        </main>   
+    </div>
+  );
+}
+
+export default DashboardWrapper
